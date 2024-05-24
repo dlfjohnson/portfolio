f63768464db60fae935e837403a33b9ee64b0529
Devon Johnson <devonjohnson@Devons-Mac-Studio.local>
2024-05-24 13:45:48 -0400

    feat: generate app and create primary components

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Header from '@/components/header';
+import "./globals.css";
+import ActiveSectionContextProvider from "@/context/active-section-context";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Devon | Personal Portfolio",
+  description: "Devon is a front-end developer with 6 years of experience",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="!scroll-smooth">
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36`}>
+        <div className="bg-[#cbd5e1] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="bg-[#e2e8f0] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <ActiveSectionContextProvider>
+          <Header />
+          {children}
+        </ActiveSectionContextProvider>
+      </body>
+    </html>
+  );
+}
