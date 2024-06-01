0fc4f599acb584249d6ae82885615608dae31832
Devon Johnson <devonjohnson@Devons-Mac-Studio.local>
2024-06-01 14:37:26 -0400

    feat: add Vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Toaster } from 'react-hot-toast';
 import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,6 +33,7 @@ export default function RootLayout({
             <Toaster position="top-right" />
             <Footer />
             <ThemeSwitch />
+            <SpeedInsights />
           </ActiveSectionContextProvider>
         </ThemeContextProvider>
       </body>
